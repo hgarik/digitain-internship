dea863e39129439d9aa63e04c4f485fec3b81953
g.a.hakobyan <g.a.hakobyan@hotmail.com>
2016-05-17 03:00:53 +0400

    pouse

--- Printer/Printer/Program.cs
+++ Printer/Printer/Program.cs
@@ -3,17 +3,44 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Threading;
+
+
 
 namespace Printer
 {
-    class MyClass
-    {
-
-    }
     class Program
     {
+        //static int i = 0;
+        //static ConsoleKeyInfo k = default(ConsoleKeyInfo);
+
+        static int printCount;
         static void Main(string[] args)
         {
+            Console.WriteLine("There is 20 Papers in the printer");
+            Console.WriteLine("Enter the cout of pages you want to print");
+            printCount = int.Parse(Console.ReadLine());
+            //Console.WriteLine("Press 'S' key to start printing");
+            Print p = new Print(printCount);
+
+            Print.Start(ref printCount);
+            if (printCount <= 0)
+            {
+                Console.WriteLine("Finished");
+                Print.IsFinished = true;
+            }
+            else if (Print.PaperCount <= 0)
+            {
+                Print.OnOutOfPaper += Print.OutOfPaper;
+                Print.OutOfPaperEvent();
+            }
+            if (Print.IsFinished)
+            {
+                Console.WriteLine("Thank you for using our printer");
+            }
+
+
+            Console.ReadKey();
         }
     }
 }
