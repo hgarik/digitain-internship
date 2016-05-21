282558c550f67076b44d579f069d2a71bce15161
g.a.hakobyan <g.a.hakobyan@hotmail.com>
2016-05-21 15:08:07 +0400

    out of papaer

---
+++ Printer1/Printer1/Printer.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Printer1
+{
+    public class Printer
+    {
+        public event EventHandler PrintStarted;
+        public event EventHandler PrintFinished;
+        public event EventHandler OutOfPaper;
+        public event EventHandler<PrintInProgressEventArgs> PrintInProgress;
+
+        public Printer(string name)
+        {
+            Name = name;
+        }
+
+        public string Name { get; set; }
+
+        public int PapersCount { get; private set; }
+
+        public void AddPapers(int count)
+        {
+            PapersCount += count;
+        }
+        protected virtual void OnOutOfPaper()
+        {
+            OutOfPaper?.Invoke(this, EventArgs.Empty);
+        }
+        public void Print(int pagesCount)
+        {
+            OnPrintStarted();
+
+            for (int i = 0; i < pagesCount; i++)
+            {
+                if (PapersCount == 0)
+                {
+                    OnOutOfPaper();
+                }
+
+                PapersCount--;
+                OnPrintInProgress(i + 1, pagesCount);
+            }
+
+            OnPrintFinished();
+        }
+
+        protected virtual void OnPrintStarted()
+        {
+            PrintStarted?.Invoke(this, EventArgs.Empty);
+        }
+
+        protected virtual void OnPrintFinished()
+        {
+            PrintFinished?.Invoke(this, EventArgs.Empty);
+        }
+
+        protected virtual void OnPrintInProgress(int printedCount, int total)
+        {
+            var e = new PrintInProgressEventArgs
+            {
+                PrintedPagesCount = printedCount,
+                PagesCount = total
+            };
+
+            PrintInProgress?.Invoke(this, e);
+        }
+    }
+}
---
+++ Printer1/Printer1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Printer1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var HP = new Printer("HP LaserJet");
+            HP.OutOfPaper += (sender, e) =>
+            {
+                Console.WriteLine($"{(sender as Printer).Name} is out of Paper \nInsert count of paper you want to add");
+                (sender as Printer).AddPapers(int.Parse(Console.ReadLine()));
+            };
+            HP.PrintStarted += (sender, e) => {
+                Thread.Sleep(1000);
+                Console.WriteLine($"{(sender as Printer)?.Name}:: Print Started"); };
+            HP.PrintFinished += (sender, e) => {
+                Thread.Sleep(1000);
+                Console.WriteLine($"{(sender as Printer)?.Name}:: Print Finished"); };
+            HP.PrintInProgress += (sender, e) => {
+                Thread.Sleep(1000);
+                Console.WriteLine($"{(sender as Printer)?.Name}:: Printing {e.PrintedPagesCount} of {e.PagesCount} [{e.Progress}%]");
+            };
+
+            HP.AddPapers(2);
+
+            HP.Print(4);
+        }
+    }
+}
